1eb3472842575c4744231bcae10e8eb67e5d2513
root <sergiovez13@gmail.com>
2026-02-09 10:56:03 +0100

    Superusuario

---
+++ finance_tracker/users/management/commands/create_default_superuser.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = "Create default superuser if it doesn't exist"
+
+    def handle(self, *args, **options):
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        if not username or not password:
+            self.stdout.write("Superuser env vars not set")
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write("Superuser already exists")
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        self.stdout.write("Superuser created successfully")
