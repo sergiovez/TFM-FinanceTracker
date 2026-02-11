fadd85ebbaff3ad03e301c0e6ee05eb01ce3e75b
root <sergiovez13@gmail.com>
2026-02-11 10:39:53 +0100

    Ver si es error SMTP

--- finance_tracker/finance_tracker/settings.py
+++ finance_tracker/finance_tracker/settings.py
@@ -167,7 +167,8 @@ if not DEBUG:
     SESSION_COOKIE_SAMESITE = "Lax"        # o "None"
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
